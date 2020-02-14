3a879c8b112f08a866aa1095868d67b0988c0491
xDuck <cbnastovski@gmail.com>
2020-02-13 20:17:17 -0500

    Added points of interest to plot.. But dont have realtime scaling

--- plotting.py
+++ plotting.py
@@ -1,24 +1,63 @@
 import json
+import numpy as np
+
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.transforms as transforms
+import matplotlib.dates as mdates
+
 from scipy.interpolate import spline
 from scipy.interpolate import interp1d
 from scipy.signal import savgol_filter
-import numpy as np
+
 from datetime import datetime
-import matplotlib.dates as mdates
 from datetime import timedelta
-from stats import get_api_data
 from dateutil import parser as date_parser
 
+from utils.stats import get_api_data
+
 api_data = get_api_data()
+game = api_data["games"][0]
 
-game_time = date_parser.parse(api_data["gameStartTimestampUTC"])
-game_start = datetime.utcfromtimestamp(1581184854.0)
-game_end = game_start + timedelta(hours=5)
+game_start = date_parser.parse(game["gameStartTimestampUTC"])
+game_end = game_start + timedelta(hours=4)
+
+SCORING_PLAYS = {"Touchdown": "%s TD",
+                 "Field Goal": "%s FG",
+                 "Interception": "%s INT6",
+                 "Fumble": "%s FR6"}
 
 
-def add_to_plot(sp1, x, y, color, label):
+def get_important_times(game, plays):
+
+    times, labels = [], []
+
+    # Check all plays for points of interest
+    for _, play in enumerate(plays):
+
+        # Scoring plays
+        if play["playScoreType"] in SCORING_PLAYS.keys():
+
+            # Compute times and labels
+            time = date_parser.parse(play["StartTime"])
+            minutes = int(play["StartTime"].split(":")[0])
+            seconds = int(play["StartTime"].split(":")[1])
+            label = SCORING_PLAYS[play["playScoreType"]] % play["ClubCode"]
+
+            # Calculate time with offset
+            offset = timedelta(minutes=15) * (play["Quarter"] - 1) + \
+                (timedelta(minutes=15) -
+                 timedelta(minutes=minutes, seconds=seconds))
+            time = game_start + offset
+            print(offset)
+
+            times.append(time)
+            labels.append(label)
+
+    return times, labels
+
+
+def add_to_plot(fig, x, y, color, label):
     xx = np.linspace(x.min(), x.max(), 1000)
     itp = interp1d(x, y, kind='linear')
     window_size, poly_order = 101, 3
@@ -27,25 +66,25 @@ def add_to_plot(sp1, x, y, color, label):
     xx_dt = [datetime.utcfromtimestamp(x) for x in xx]
     dates = matplotlib.dates.date2num(xx_dt)
 
-    sp1.plot_date(dates, yy, color, linewidth=3, label=label)
+    fig.plot_date(dates, yy, color, linewidth=3, label=label)
 
 
 # Create a canvas to place the subgraphs
 canvas = plt.figure()
 rect = canvas.patch
 rect.set_facecolor('white')
+fig = canvas.subplots()
 
-# Define the matrix of 1x1 to place subplots
-# Placing the plot1 on 1x1 matrix, at pos 1
-sp1 = canvas.add_subplot(1, 1, 1)
+# Axis formatting
+myFmt = mdates.DateFormatter('%H:%M')
+fig.xaxis.set_major_formatter(myFmt)
+trans = transforms.blended_transform_factory(
+    fig.axes.transData, fig.axes.transAxes)
 
 # Put the title and labels
-sp1.set_title('STL v DC')
-sp1.set_xlabel('Game Time')
-sp1.set_ylabel('Fanbase Sentiment')
-
-myFmt = mdates.DateFormatter('%H:%M')
-sp1.xaxis.set_major_formatter(myFmt)
+fig.set_title('STL v DC')
+fig.set_xlabel('Game Time')
+fig.set_ylabel('Fanbase Sentiment')
 
 with open('team1.json') as json_file:
     data = json.load(json_file)
@@ -53,7 +92,7 @@ x = np.array([x for x, _ in data if datetime.utcfromtimestamp(
     x) > game_start and datetime.utcfromtimestamp(x) < game_end])
 y = np.array([y for x, y in data if datetime.utcfromtimestamp(
     x) > game_start and datetime.utcfromtimestamp(x) < game_end])
-add_to_plot(sp1, x, y, 'black', 'DC')
+add_to_plot(fig, x, y, 'black', 'DC')
 
 with open('team2.json') as json_file:
     data = json.load(json_file)
@@ -61,15 +100,15 @@ x = np.array([x for x, _ in data if datetime.utcfromtimestamp(
     x) > game_start and datetime.utcfromtimestamp(x) < game_end])
 y = np.array([y for x, y in data if datetime.utcfromtimestamp(
     x) > game_start and datetime.utcfromtimestamp(x) < game_end])
-add_to_plot(sp1, x, y, 'red', 'STL')
+add_to_plot(fig, x, y, 'red', 'STL')
 
-with open('other.json') as json_file:
-    data = json.load(json_file)
-x = np.array([x for x, _ in data if datetime.utcfromtimestamp(
-    x) > game_start and datetime.utcfromtimestamp(x) < game_end])
-y = np.array([y for x, y in data if datetime.utcfromtimestamp(
-    x) > game_start and datetime.utcfromtimestamp(x) < game_end])
-# add_to_plot(sp1, x, y, 'green')
+# Add important points
+times, labels = get_important_times(game, api_data["playList"]["plays"])
+
+for time, label in zip(times, labels):
+    print(time, label)
+    plt.axvline(time)
+    plt.text(time, +0.1, label, transform=trans)
 
 # Show the plot/image
 plt.legend(loc="upper left")
