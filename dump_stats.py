bc6afa52824c5ac762ae16cc1d903d72a1f62a15
xDuck <cbnastovski@gmail.com>
2020-02-19 21:42:11 -0500

    Made data dump more consolidated

--- dump_stats.py
+++ dump_stats.py
@@ -1,4 +1,62 @@
+import csv
 from utils import stats
 
-for i in range(0, 9):
-    stats.stats_as_csv(game_id=i)
+tt_headers = []
+tt_csv = []
+tt_f = csv.writer(open("team_totals.csv", "w+"))
+
+o_headers = []
+o_csv = []
+o_f = csv.writer(open("offense.csv", "w+"))
+
+d_headers = []
+d_csv = []
+d_f = csv.writer(open("defense.csv", "w+"))
+
+p_headers = []
+p_csv = []
+p_f = csv.writer(open("plays.csv", "w+"))
+
+po_headers = []
+po_csv = []
+po_f = csv.writer(open("possessions.csv", "w+"))
+
+game_id = 1
+while True:
+    print("Getting Game: %d" % game_id)
+    data = stats.stats_as_csv(game_id=game_id)
+    game_id += 1
+    if data is None:
+        break
+
+    tt, o, d, p, po = data
+
+    tt_headers = tt[0]
+    tt_csv += tt[1]
+
+    o_headers = o[0]
+    o_csv += o[1]
+
+    d_headers = d[0]
+    d_csv += d[1]
+
+    p_headers = p[0]
+    p_csv += p[1]
+
+    po_headers = po[0]
+    po_csv += po[1]
+
+tt_f.writerow(tt_headers)
+tt_f.writerows(tt_csv)
+
+o_f.writerow(o_headers)
+o_f.writerows(o_csv)
+
+d_f.writerow(d_headers)
+d_f.writerows(d_csv)
+
+p_f.writerow(p_headers)
+p_f.writerows(p_csv)
+
+po_f.writerow(po_headers)
+po_f.writerows(po_csv)
--- utils/stats.py
+++ utils/stats.py
@@ -47,30 +47,32 @@ def stats_as_csv(game_id=1):
     with open("data.json", "w+") as f:
         json.dump(data, f, indent=2)
 
-    home_team = data["games"][game_id]["homeTeamAbbr"]
-    away_team = data["games"][game_id]["awayTeamAbbr"]
+    games = {}
+    for game in data["games"]:
+        games[game["gameId"]] = game
+    if (game_id not in games) or (not games[game_id]["isGameOver"]):
+        print((game_id not in games), (not games[game_id]["isGameOver"]))
+        return None
+
+    home_team = games[game_id]["homeTeamAbbr"]
+    away_team = games[game_id]["awayTeamAbbr"]
     prefix = away_team + "_" + home_team
 
     # Team Total Stats
-    f = csv.writer(open(prefix + "_team.csv", "w+"))
-
-    headers = ["TeamName"] + list(data["teamStats"]["awayStats"]["stats"].keys())
-    away = [away_team] + list(data["teamStats"]["awayStats"]["stats"].values())
-    home = [home_team] + list(data["teamStats"]["homeStats"]["stats"].values())
-
-    f.writerow(headers)
-    f.writerow(away)
-    f.writerow(home)
+    tt_headers = ["GameID", "TeamName"] + list(data["teamStats"]["awayStats"]["stats"].keys())
+    tt_away = [game_id, away_team] + list(data["teamStats"]["awayStats"]["stats"].values())
+    tt_home = [game_id, home_team] + list(data["teamStats"]["homeStats"]["stats"].values())
 
     def get_team_stat(csv, stats, statData, team, teamName):
 
+        headers = ["TeamName", "PlayerName", "PlayerNumber"]
         players = []
         for _, stat in enumerate(stats):
             players += [entry["player"]["id"] for entry in statData[team][stat]]
-            players += [entry["player"]["id"] for entry in statData[team][stat]]
-            players += [entry["player"]["id"] for entry in statData[team][stat]]
+            headers += [stat.capitalize() + key.capitalize() for key in statData[team][stat][0].keys()]
+            headers.remove(stat.capitalize() + "Player")
+        
         players = set(players)
-
         player_data = {}
         for player in players:
             player_data[player] = {}
@@ -89,60 +91,52 @@ def stats_as_csv(game_id=1):
 
         out = []
         for player in players:
-            pdata = []
+            pdata = [game_id]
             for _, key in enumerate(headers):
                 pdata += [player_data[player][key]]
             out += [pdata]
-        csv.writerows(out)
+        return out
 
     # Offensive stats
-    f = csv.writer(open(prefix + "_offense.csv", "w+"))
-
     rushing_headers = list(data["offensiveStats"]["away"]["rushing"][0].keys())
     passing_headers = list(data["offensiveStats"]["away"]["passing"][0].keys())
     receiving_headers = list(data["offensiveStats"]["away"]["receiving"][0].keys())
 
-    headers = ["TeamName", "PlayerName", "PlayerNumber"]
-    headers += ["Rushing" + k.capitalize() for k in rushing_headers]
-    headers += ["Passing" + k.capitalize() for k in passing_headers]
-    headers += ["Receiving" + k.capitalize() for k in receiving_headers]
-    headers.remove("RushingPlayer")
-    headers.remove("PassingPlayer")
-    headers.remove("ReceivingPlayer")
+    o_headers = ["GameID", "TeamName", "PlayerName", "PlayerNumber"]
+    o_headers += ["Rushing" + k.capitalize() for k in rushing_headers]
+    o_headers += ["Passing" + k.capitalize() for k in passing_headers]
+    o_headers += ["Receiving" + k.capitalize() for k in receiving_headers]
+    o_headers.remove("RushingPlayer")
+    o_headers.remove("PassingPlayer")
+    o_headers.remove("ReceivingPlayer")
 
-    f.writerow(headers)
-    get_team_stat(f, ["rushing", "passing", "receiving"], data["offensiveStats"], "away", away_team)
-    get_team_stat(f, ["rushing", "passing", "receiving"], data["offensiveStats"], "home", home_team)
+    o_away = get_team_stat(f, ["rushing", "passing", "receiving"], data["offensiveStats"], "away", away_team)
+    o_home = get_team_stat(f, ["rushing", "passing", "receiving"], data["offensiveStats"], "home", home_team)
 
     # Defensive stats
-    f = csv.writer(open(prefix + "_defense.csv", "w+"))
-
     defensive_headers = list(data["defensiveStats"]["away"]["defensive"][0].keys())
 
-    headers = ["TeamName", "PlayerName", "PlayerNumber"]
-    headers += ["Defensive" + k.capitalize() for k in defensive_headers]
-    headers.remove("DefensivePlayer")
+    d_headers = ["GameID", "TeamName", "PlayerName", "PlayerNumber"]
+    d_headers += ["Defensive" + k.capitalize() for k in defensive_headers]
+    d_headers.remove("DefensivePlayer")
 
-    f.writerow(headers)
-    get_team_stat(f, ["defensive"], data["defensiveStats"], "away", away_team)
-    get_team_stat(f, ["defensive"], data["defensiveStats"], "home", home_team)
+    d_away = get_team_stat(f, ["defensive"], data["defensiveStats"], "away", away_team)
+    d_home = get_team_stat(f, ["defensive"], data["defensiveStats"], "home", home_team)
 
     # Plays
-    f = csv.writer(open(prefix + "_plays.csv", "w+"))
+    p_headers = ["GameID"] + list(data["playList"]["plays"][0].keys())
 
-    headers = list(data["playList"]["plays"][0].keys())
-    f.writerow(headers)
-
+    p_rows = []
     for play in data["playList"]["plays"]:
-        f.writerow(list(play.values()))
+        p_rows.append([game_id] + list(play.values()))
 
     # Possessions
-    f = csv.writer(open(prefix + "_possessions.csv", "w+"))
+    po_headers = ["GameID"] + list(data["possessionStats"]["away"]["possessions"][0].keys())
 
-    headers = list(data["possessionStats"]["away"]["possessions"][0].keys())
-    f.writerow(headers)
-
+    po_rows = []
     for poss in data["possessionStats"]["away"]["possessions"] + data["possessionStats"]["home"]["possessions"]:
-        f.writerow(list(poss.values()))
+        po_rows.append([game_id] + list(poss.values()))
+
+    return (tt_headers, [tt_away] + [tt_home]), (o_headers, o_away + o_home), (d_headers, d_away + d_home), (p_headers, p_rows), (po_headers, po_rows)
 
 
